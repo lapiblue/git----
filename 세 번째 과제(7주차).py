e88194cc794427bbfc88ce8dbcb6920dab11afbb
keumhyun <keumjul06@gmail.com>
2025-10-16 12:10:42 +0900

    과제#14

--- 세 번째 과제(7주차).py
+++ 세 번째 과제(7주차).py
@@ -9,7 +9,12 @@
 # x=x[:-2]
 # print(x)
 
-#과제13
-x=input('숫자5개 입력:').split()
-for index, value in enumerate(x, start=101):
-    print(index, value)
+# #과제13
+# x=input('숫자5개 입력:').split()
+# for index, value in enumerate(x, start=101):
+#     print(index, value)
+
+#과제14
+a=[10,20,30,40,30,20,10]
+b=a.count(20)
+print(b)
