f13a6ab548a1c8b338c58c7fb44a0b70bd7da56c
keumhyun <keumjul06@gmail.com>
2025-10-16 13:25:11 +0900

    과제#15

--- 세 번째 과제(7주차).py
+++ 세 번째 과제(7주차).py
@@ -1,20 +1,27 @@
-# #과제11
-# x=input('숫자5개 입력:').split()
-# y=input('숫자1개 입력:')
-# x.append(y)
-# print(x)
+#과제11
+x=input('숫자5개 입력:').split()
+y=input('숫자1개 입력:')
+x.append(y)
+print(x)
 
-# #과제12
-# x=input('숫자5개 입력:').split()
-# x=x[:-2]
-# print(x)
+#과제12
+x=input('숫자5개 입력:').split()
+x=x[:-2]
+print(x)
 
-# #과제13
-# x=input('숫자5개 입력:').split()
-# for index, value in enumerate(x, start=101):
-#     print(index, value)
+#과제13
+x=input('숫자5개 입력:').split()
+for index, value in enumerate(x, start=101):
+    print(index, value)
 
 #과제14
 a=[10,20,30,40,30,20,10]
 b=a.count(20)
 print(b)
+
+#과제15
+x=input('숫자10개 입력:').split()
+y=min(x)
+z=max(x)
+
+print(y,'and',z)
